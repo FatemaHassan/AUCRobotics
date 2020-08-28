5b73912cd0c8aeb7a87d43b9647f810f1162ace0
Fatema Hassan <fhaman2010@gmail.com>
2020-08-28 16:06:18 +0200

    pictures added

--- Day3/tasks/task3/catkin_ws/build/CTestTestfile.cmake
+++ Day3/tasks/task3/catkin_ws/build/CTestTestfile.cmake
@@ -5,6 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
-subdirs("src/my_robot")
-subdirs("src/ball_chaser")
 subdirs("AUCRobotics_ws")
